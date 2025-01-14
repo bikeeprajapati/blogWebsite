27caf65b356c29346149f4fb03e4384c5f817a2c
Bikee Prajapati <bikeeprajapati1@gmail.com>
2025-01-14 23:54:10 +0545

    Work on nav bar

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse('index page')
-def about(request):
-    return HttpResponse('About page')
+    return render(request, 'blog/index.html')
