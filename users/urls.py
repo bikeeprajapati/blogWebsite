f353a86c25d03a91287bbbe340acbe290b7110bc
Bikee Prajapati <bikeeprajapati1@gmail.com>
2025-01-18 22:58:42 +0545

    Worked on logout page

--- users/urls.py
+++ users/urls.py
@@ -1,6 +1,12 @@
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_view
+from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
     path('sign_up/', views.sign_up, name='users-sign_up'),
+    path('login/', auth_view.LoginView.as_view(template_name='users/login.html'), name='users-login'),
+    
+    # Logout URL path
+    path('logout/', LogoutView.as_view(template_name='logout.html'), name='logout'),
 ]
