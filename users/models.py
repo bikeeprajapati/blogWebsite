f02972941cfb6bb7fe12f32cf13aac4975da697e
Bikee Prajapati <bikeeprajapati1@gmail.com>
2025-01-20 18:04:14 +0545

    worked on user profile

--- users/models.py
+++ users/models.py
@@ -5,7 +5,7 @@ from django.core.validators import FileExtensionValidator
 # Create your models here.
 class ProfileModel(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    image =models.ImageField(default='default.png', upload_to='profile', validators=[FileExtensionValidator(['png','jpg'])])
+    image =models.ImageField(default='image.png', upload_to='profile', validators=[FileExtensionValidator(['png','jpg'])])
     bio = models.TextField(max_length=500, blank=True)
     
     def __str__(self):
